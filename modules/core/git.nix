b0c24fe3b415d8593985e0ec02fbbca688011e01
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-03-15 15:11:08 +0700

    feat(git): add git-credential-manager for GitLab support
    - Configure GCM as the global credential helper
    - Ensure 'gh' is still used for GitHub and Gist hosts via host-specific config
    - Clean up unsupported GitLab hosts from 'gh' configuration

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -1,18 +1,35 @@
 {
   core.git = {settings, ...}: {
-    homeManager.programs = {
-      gh = {
-        enable = true;
-        gitCredentialHelper.enable = true;
-      };
-      difftastic = {
-        enable = true;
-        git.enable = true;
-        git.diffToolMode = true;
-      };
-      git = {
-        enable = true;
-        inherit settings;
+    homeManager = {pkgs, ...}: {
+      programs = {
+        gh = {
+          enable = true;
+          gitCredentialHelper = {
+            enable = true;
+            hosts = [
+              "https://github.com"
+              "https://gist.github.com"
+            ];
+          };
+        };
+        difftastic = {
+          enable = true;
+          git.enable = true;
+          git.diffToolMode = true;
+        };
+        git = {
+          enable = true;
+          inherit settings;
+          extraConfig = {
+            # Use GCM as the default (handles GitLab, etc.)
+            credential.helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
+            credential.credentialStore = "secretservice";
+
+            # Explicitly use 'gh' for GitHub/Gist hosts
+            "credential \"https://github.com\"".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+            "credential \"https://gist.github.com\"".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+          };
+        };
       };
       lazygit = {
         enable = true;
@@ -46,6 +63,7 @@
           };
         };
       };
+      home.packages = [pkgs.git-credential-manager];
     };
   };
 }
