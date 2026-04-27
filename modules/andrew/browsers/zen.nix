e8fbb5b9d7cee598502d602c2b1c9c2f3d908de7
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-04-27 20:14:07 +0700

    chore(zen): add workspace extension ids

--- modules/andrew/browsers/zen.nix
+++ modules/andrew/browsers/zen.nix
@@ -28,6 +28,8 @@
           "4c2bec61-7f6c-4e5c-bdc6-c9ad1aba1827"
           "378ba8b9-cd36-45f5-88df-595df5288795"
           "7d577b21-4685-4db2-bb17-d39d08eec199"
+          "5bb07b6e-c89f-4f4a-a0ed-e483cc535594"
+          "c01d3e22-1cee-45c1-a25e-53c0f180eea8"
         ];
         settings = {
           "zen.workspaces.continue-where-left-off" = true;
