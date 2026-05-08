ed06eaf420fd4cb7c808fd46842f31e54e43d4cc
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-05-08 11:27:31 +0700

    chore(laptop): expand charging threshold

--- modules/core/power-manager.nix
+++ modules/core/power-manager.nix
@@ -27,7 +27,7 @@
 
           # Battery Charge Thresholds
           START_CHARGE_THRESH_BAT0 = 40;
-          STOP_CHARGE_THRESH_BAT0 = 80;
+          STOP_CHARGE_THRESH_BAT0 = 85;
           RESTORE_THRESHOLDS_ON_BAT = 1;
 
           # PCIE & Runtime PM
