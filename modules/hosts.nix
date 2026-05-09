a2030a6e29ab05fa860114cf24b03391c626cf20
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-05-09 20:56:58 +0700

    refactor(hosts): define terminal + gdrive-path per host

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,17 +1,33 @@
 {__findFile, ...}: let
   arch = "x86_64-linux";
+  terminal = {
+    fontSize = 12;
+    padding = 2;
+    opacity = 0.8;
+  };
+  gdrive-path = "/mnt/gdrive";
 in {
   den.hosts.${arch} = {
-    andrew-laptop.users.andrew = {};
-    andrew-pc.users.andrew = {};
+    andrew-laptop = {
+      inherit terminal;
+      inherit gdrive-path;
+      users.andrew = {};
+    };
+    andrew-pc = {
+      inherit terminal;
+      inherit gdrive-path;
+      users.andrew = {};
+    };
     andrew-home-wsl = {
       wsl.enable = true;
       users.andrew = {};
+      inherit terminal;
       windowsName = "hoanganh";
     };
     andrew-work-wsl = {
       wsl.enable = true;
       users.andrew = {};
+      inherit terminal;
       windowsName = "andrew.nguyen1";
     };
   };
