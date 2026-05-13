50e2c8bb95e3e43ca145de3686e4223fc75667b6
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-05-13 14:35:11 +0000

    chore(core): make worsktation stays the same
    So if we need we can just copy and paste from `/etc/nixos/` without
    shoot our-self in the foot

--- hosts/andrew-laptop/_nixos/hardware-configuration.nix
+++ hosts/andrew-laptop/_nixos/hardware-configuration.nix
@@ -20,6 +20,21 @@
     extraModulePackages = [];
   };
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/42961c1a-eb94-4384-945a-1b5bb54d6f14";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/99E9-7D91";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/036fbd89-d74d-4782-ab66-b58f64a6b169";}
+  ];
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- hosts/andrew-pc/_nixos/hardware-configuration.nix
+++ hosts/andrew-pc/_nixos/hardware-configuration.nix
@@ -20,6 +20,21 @@
     extraModulePackages = [];
   };
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/b8589191-3b30-4cb9-bf2a-54164fff9325";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/D98A-14CA";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/e2e35b8b-aca6-42bf-9c74-7fd819efbda7";}
+  ];
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
