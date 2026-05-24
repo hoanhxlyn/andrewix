9ddfdfd8e5505541c45171656c9f1b271bad7613
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-05-24 20:18:09 +0700

    hotfix(code): split vscode and cursor settings

--- modules/my/editor/cursor.nix
+++ modules/my/editor/cursor.nix
@@ -1,12 +1,21 @@
-{__findFile, ...}: {
+{
+  lib,
+  self,
+  __findFile,
+  ...
+}: {
   den.aspects.my.editor.cursor = {
     includes = [
       (<den.batteries.unfree> ["cursor"])
     ];
-    nixos = {pkgs, ...}: {
-      environment.systemPackages = [
-        pkgs.code-cursor
-      ];
+    homeManager = {pkgs, ...}: {
+      home.packages = [pkgs.code-cursor];
+
+      home.file.".config/Cursor/User/settings.json".text =
+        builtins.toJSON (lib.importJSON "${self}/config/cursor/settings.json");
+
+      home.file.".config/Cursor/User/keybindings.json".text =
+        builtins.toJSON (lib.importJSON "${self}/config/cursor/keybindings.json");
     };
   };
 }
