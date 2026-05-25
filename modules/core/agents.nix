3fbeca9dd42138bf92001baa9efb8d3487ec52c1
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-05-25 13:44:35 +0700

    feat(agents): add claude

--- modules/core/agents.nix
+++ modules/core/agents.nix
@@ -1,71 +1,80 @@
-{
-  core.agents.homeManager = {
-    programs = {
-      uv.enable = true;
-      mcp = {
-        enable = true;
-        servers = {
-          context7 = {
-            url = "https://mcp.context7.com/mcp";
-            headers = {
-              CONTEXT7_API_KEY = "{env:CONTEXT7_API_KEY}";
+{__findFile, ...}: {
+  core.agents = {
+    includes = [
+      (<den/batteries/unfree> ["claude-code"])
+    ];
+    homeManager = {
+      programs = {
+        uv.enable = true;
+        mcp = {
+          enable = true;
+          servers = {
+            context7 = {
+              url = "https://mcp.context7.com/mcp";
+              headers = {
+                CONTEXT7_API_KEY = "{env:CONTEXT7_API_KEY}";
+              };
             };
-          };
-          tavily = {
-            command = "bunx";
-            args = [
-              "tavily-mcp@latest"
-            ];
-            env = {
-              TAVILY_API_KEY = "{env:TAVILY_API_KEY}";
+            tavily = {
+              command = "bunx";
+              args = [
+                "tavily-mcp@latest"
+              ];
+              env = {
+                TAVILY_API_KEY = "{env:TAVILY_API_KEY}";
+              };
             };
-          };
-          deepwiki = {
-            command = "pnpm";
-            args = [
-              "dlx"
-              "mcp-deepwiki@latest"
-            ];
+            deepwiki = {
+              command = "pnpm";
+              args = [
+                "dlx"
+                "mcp-deepwiki@latest"
+              ];
+            };
           };
         };
-      };
-      opencode = {
-        enable = true;
-        enableMcpIntegration = true;
-        settings = {
-          username = "😺 Andrew Nguyen";
-          autoupdate = false;
-          permission = {
-            webfetch = "deny";
-            websearch = "deny";
-          };
-          lsp = {
-            nix = {
-              command = ["nil"];
-              extensions = [".nix"];
+        opencode = {
+          enable = true;
+          enableMcpIntegration = true;
+          settings = {
+            username = "😺 Andrew Nguyen";
+            autoupdate = false;
+            permission = {
+              webfetch = "deny";
+              websearch = "deny";
+            };
+            lsp = {
+              nix = {
+                command = ["nil"];
+                extensions = [".nix"];
+              };
+            };
+            formatter = {
+              nixfmt = {
+                disabled = true;
+              };
+              alejandra = {
+                command = [
+                  "alejandra"
+                  "$FILE"
+                ];
+                extensions = [".nix"];
+              };
+            };
+            plugin = [
+              "opencode-agent-skills"
+            ];
+            command = {
+              commit = {
+                description = "Auto generate commit message";
+                template = "Generate a git convention message for changes. DO NOT commit them !";
+              };
             };
           };
-          formatter = {
-            nixfmt = {
-              disabled = true;
-            };
-            alejandra = {
-              command = [
-                "alejandra"
-                "$FILE"
-              ];
-              extensions = [".nix"];
-            };
-          };
-          plugin = [
-            "opencode-agent-skills"
-          ];
-          command = {
-            commit = {
-              description = "Auto generate commit message";
-              template = "Generate a git convention message for changes. DO NOT commit them !";
-            };
-          };
+        };
+        claude-code = {
+          enable = true;
+          enableMcpIntegration = true;
         };
       };
     };
