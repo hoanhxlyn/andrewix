996cda8de15208db25f02520ef8cbbf7a6482098
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-06-25 16:54:28 +0700

    hotfix(node): still need full version

--- modules/my/cli/node.nix
+++ modules/my/cli/node.nix
@@ -2,7 +2,7 @@
   den.aspects.my.cli.node = {
     homeManager = {pkgs, ...}: {
       home.packages = with pkgs; [
-        nodejs-slim_latest
+        nodejs_latest
         # nodejs_22
         # pnpm
       ];
