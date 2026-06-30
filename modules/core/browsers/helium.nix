92d3873b1d485bcd5749714307f7a32789762566
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-06-30 14:07:47 +0700

    feat(helium): add settings

--- modules/core/browsers/helium.nix
+++ modules/core/browsers/helium.nix
@@ -22,6 +22,7 @@
           "SpellcheckLanguage" = ["en-US"];
           # "DefaultSearchProviderEnabled" = true;
           # "DefaultSearchProviderSearchURL" = "https://search.nixos.org/?q={searchTerms}";
+          "ShowHomeButton" = true;
           "ExtensionInstallForcelist" = [
             # Pre-install extensions
             "oboonakemofpalcgghocfoadofidjkkk"
@@ -29,6 +30,36 @@
           ];
         };
       };
+    };
+    homeManager = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      home.activation.heliumPreferences = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        PREFS="$HOME/.config/net.imput.helium/Default/Preferences"
+        if [ -f "$PREFS" ]; then
+          TMP=$(mktemp)
+          ${pkgs.jq}/bin/jq '. * {
+            "helium": {
+              "browser": {
+                "centered_location_bar": true,
+                "layout": 2,
+                "minimal_location_bar": true,
+                "new_tab_next_to_active": true,
+                "vertical_right_aligned": false,
+                "zen_mode": true,
+                "zen_mode_sidebar_pinned": true,
+                "zen_mode_top_chrome_pinned": true
+              }
+            },
+            "browser": {
+              "show_home_button": true,
+              "theme": { "is_grayscale2": true }
+            }
+          }' "$PREFS" > "$TMP" && mv "$TMP" "$PREFS"
+        fi
+      '';
     };
   };
 }
