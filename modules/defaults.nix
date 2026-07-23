a460f29be0759393120f1bfd882242ff27f56dd3
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-07-23 11:08:58 +0700

    feat(direnv): add gum for extra goodies

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -32,9 +32,14 @@
     packages = den.lib.nh.denPackages {fromFlake = true;} pkgs;
     devShells.default = pkgs.mkShell {
       packages = devPackages;
-      shellHook = ''
-        echo "Loaded dedicated plugins:"
-        ${lib.concatMapStrings (p: "echo \"- ${p.pname or p.name}\"\n") devPackages}
+      shellHook = let
+        gum = lib.getExe pkgs.gum;
+        list = lib.concatMapStringsSep "\n" (p: "• ${p.pname or p.name}") devPackages;
+      in ''
+        ${gum} style \
+          --border rounded --border-foreground 212 --padding "0 2" \
+          "$(${gum} style --foreground 212 --bold "Loaded dedicated plugins")" \
+          "${list}"
       '';
     };
   };
