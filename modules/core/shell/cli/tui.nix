841dd55af3f870848e95acda92c8a1f6b11ed8cf
Andrew Nguyen <hoanhxlyn@gmail.com>
2026-08-16 15:14:03 +0700

    chore(sql): remove sqlit

--- modules/core/shell/cli/tui.nix
+++ modules/core/shell/cli/tui.nix
@@ -1,7 +1,7 @@
 {__findFile, ...}: {
   core.cli.tui = {
     homeManager = {pkgs, ...}: {
-      home.packages = with pkgs; [sqlit-tui bruno];
+      home.packages = with pkgs; [bruno];
       programs.dbeaver.enable = true;
       # xdg.configFile."YouTube Music/config.json".force = true;
       # xdg.configFile."YouTube Music/config.json".source = "${self}/config/youtube-music/config.json";
